2a1c207c9aedd15da55d0bfbaac7c45b99f8dcda
carsten13290 <161488325+carsten13290@users.noreply.github.com>
2024-07-26 14:16:43 +0700

    adjust

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,5 @@
 import bcryptjs from 'bcryptjs';
-import Listing from '../models/Listing.model.js';
+import Listing from '../models/listing.model.js';
 import User from '../models/user.model.js';
 import { errorHandler } from '../utils/error.js';
 
